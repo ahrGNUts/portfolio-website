6cac7ae021a7da226fffed62b39199bda5875725
Patrick <revmydev@gmail.com>
2019-04-08 16:21:04 -0700

    skeleton components + navbar entries for new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { BooksComponent } from './books/books.component';
+import { FilmComponent } from './film/film.component';
+import { ContactComponent } from './contact/contact.component';
 import { NgModule } from '@angular/core';
 
 const appRoutes = [
   { path: '', redirectTo: '/about', pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
-
+  { path: 'books', component: BooksComponent },
+  { path: 'film', component: FilmComponent },
+  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AboutComponent } from './about/about.component';
+import { BooksComponent } from './books/books.component';
+import { FilmComponent } from './film/film.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavbarComponent,
-    AboutComponent
+    AboutComponent,
+    BooksComponent,
+    FilmComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
