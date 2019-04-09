3dd1d06c7038a7cad9dd3704d4f45cdf902274a2
Patrick <revmydev@gmail.com>
2019-04-09 12:46:44 -0700

    logic/display for books component; pointer for book icons

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Book } from '../models/book.model';
+import { BooksService } from '../services/books.service';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.css']
+  styleUrls: ['./books.component.css'],
+  providers: [BooksService]
 })
-export class BooksComponent implements OnInit {
+export class BooksComponent {
+  books: Book[] = this.booksService.getBooks();
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(private booksService: BooksService) { }
 
 }
