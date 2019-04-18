b22f1f9303303b4b0a5dd966ded91d8411fb1760
Patrick <revmydev@gmail.com>
2019-04-18 11:56:18 -0700

    fleshed out film component

--- src/app/film/film.component.ts
+++ src/app/film/film.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Film } from '../models/film.model';
+import { FilmsService } from '../services/films.service';
+import { ModalService } from '../services/modal.service';
 
 @Component({
   selector: 'app-film',
   templateUrl: './film.component.html',
-  styleUrls: ['./film.component.css']
+  styleUrls: ['./film.component.css'],
+  providers: [FilmsService]
 })
 export class FilmComponent implements OnInit {
 
-  constructor() { }
+  films: Film[] = this.filmsService.getFilms();
+
+  constructor(private filmsService: FilmsService,
+              private modalService: ModalService) { }
 
   ngOnInit() {
+  }
+
+  filmSelected(film: Film) {
+    this.modalService.passFilmInfo(film);
   }
 
 }
