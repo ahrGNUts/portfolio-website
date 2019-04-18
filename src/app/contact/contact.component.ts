abb3246f768c6d2f9d25a052f0cd66116095ffd9
Patrick <revmydev@gmail.com>
2019-04-18 13:29:30 -0700

    added character counter to message textarea

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -10,6 +10,15 @@ export class ContactComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(() => {
+      let $message = $('#message');
+      var charCount = 0;
+
+      $message.on('input', () => {
+        charCount = $message.val().length;
+        $('#charCounter').html(charCount + '/2000');
+      });
+    });
   }
 
 }
