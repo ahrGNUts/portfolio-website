6bcba92d8fbed023488ae3e596824fcd5792eb4c
Patrick <revmydev@gmail.com>
2019-04-18 11:56:49 -0700

    updated modal service

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,10 +1,16 @@
 import { Book } from '../models/book.model';
+import { Film } from '../models/film.model';
 import { EventEmitter } from '@angular/core';
 
 export class ModalService {
   bookSelected = new EventEmitter<Book>();
+  filmSelected = new EventEmitter<Film>();
 
   passBookInfo(book: Book) {
     this.bookSelected.emit(book);
+  }
+
+  passFilmInfo(film: Film) {
+    this.filmSelected.emit(film);
   }
 }
